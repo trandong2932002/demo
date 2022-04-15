bcd41e0be71e3678deb4b75d7758bfb5581be385
Tran Dong <trandong2932002@gmail.com>
2022-04-15 18:48:01 +0700

    Database name error

--- crm/settings.py
+++ crm/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
+        'NAME': 'py_crm',
         'USER': 'postgres',
         'PASSWORD': '2932002',
         'HOST': 'localhost',
